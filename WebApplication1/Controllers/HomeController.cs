f77c7ad5d73755824b8b6c8a1e1571604d1bb7cb
netamateur <rita.h.tse@gmail.com>
2018-01-18 13:32:20 +1100

    viewmodel added

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,20 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
 using WebApplication1.Services;
+using WebApplication1.ViewModels;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
         private IRestaurantData _restaurantData;
+        private IGreeter _greeter;
 
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(IRestaurantData restaurantData,
+                               IGreeter greeter)
         {
             _restaurantData = restaurantData;
+            _greeter = greeter;
         }
         public IActionResult Index()
         {
-            var model = _restaurantData.GetAll();
+            var model = new HomeIndexViewModel();
+            model.Restaurants = _restaurantData.GetAll();
+            model.CurrentMessage = _greeter.GetMessageOftheDay();
 
             //returns object in json
             //return new ObjectResult(model);

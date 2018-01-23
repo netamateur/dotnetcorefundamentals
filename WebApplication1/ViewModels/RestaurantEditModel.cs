eb955c944c653ab16d0ff89f0fa1151404d703b8
netamateur <rita.h.tse@gmail.com>
2018-01-23 15:10:36 +1100

    finished models section

--- WebApplication1/ViewModels/RestaurantEditModel.cs
+++ WebApplication1/ViewModels/RestaurantEditModel.cs
@@ -1,10 +1,13 @@
-﻿using WebApplication1.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using WebApplication1.Models;
+
 
 namespace WebApplication1.ViewModels
 {
     public class RestaurantEditModel
     {
 
+        [Required, MaxLength(80)]
         public string Name { get; set;  }
         public CuisineType Cuisine { get; set; }
         

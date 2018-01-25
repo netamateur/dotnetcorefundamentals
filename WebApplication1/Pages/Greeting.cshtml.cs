306076ff244e7052f47b084bd1391584181a113c
netamateur <rita.h.tse@gmail.com>
2018-01-25 11:28:38 +1100

    implemented some razor pages

---
+++ WebApplication1/Pages/Greeting.cshtml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Services;
+
+namespace WebApplication1.Pages
+{
+    public class GreetingModel : PageModel
+    {
+        private IGreeter _greeter;
+
+        public string CurrentGreeting { get; set; }
+
+        public GreetingModel(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+        public void OnGet(string name)
+        {
+            CurrentGreeting = $"{name}: {_greeter.GetMessageOftheDay()}";
+        }
+    }
+}
